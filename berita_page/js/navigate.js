6db4bbca57f26863ed22043a22d80b498d08a895
C4ptainN3mo <endra1632@gmail.com>
2022-08-19 17:37:35 +0700

    P

---
+++ berita_page/js/navigate.js
@@ -0,0 +1,15 @@
+function navigateDetail() {
+  location.href = "../berita_page/detail.html";
+}
+
+function navigateHome() {
+  location.href = "../Dashboard.html";
+}
+
+function navigateBerita() {
+  location.href = "../Berita.html";
+}
+
+function navigateGalery() {
+  location.href = "../galeri.html";
+}
